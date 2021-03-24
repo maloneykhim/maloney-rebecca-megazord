ee2cfcdcc54096f6573d05a860346e78a8806c28
maloneykhim <70386330+maloneykhim@users.noreply.github.com>
2021-03-23 22:26:11 -0400

    edits

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -1,29 +1,46 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: false,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
 
-    // And if we need pagination
-    pagination: {
-      el: '.swiper-pagination-bullets',
-      type: 'bullets',
-      clickable: true,
-    },
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
   
-  });
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+let banners = document.querySelectorAll('.banner');
+
+banners.forEach(banner => {
+  const image = banner.querySelector('.image');
+  const title = banner.querySelector('.title');
+  const contenu = banner.querySelector('.contenu');
+  
+  gsap.timeline({
+    scrollTrigger: {
+      markers: true,
+      start: 'bottom bottom',
+      trigger: banner,
+      toggleActions: 'play none none none',
+    }
+  })
+  .from(image, {scale: 2})
+  .from(title, {y: 100, opacity: 0})
+  .from(contenu, {opacity: 0})
+});
