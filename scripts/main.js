312dd504d13b78efe2b10ae9f13399e55cf6b1fa
maloneykhim <70386330+maloneykhim@users.noreply.github.com>
2021-05-16 19:43:34 -0400

    changements

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,69 @@
 // let questions = [
-//   {
-//     q: "D'où viennent les membres du groupe Marianas Trench?",
-//     o1: "Vancouver, Colombie-Britannique",
-//     o2: "Montréal, Québec",
-//     o3: "Los Angeles, Californie",
-//     o4: "St-Petersburg, Floride",
-//     r: 1,
+// {
+//      q: "D'où viennent les membres du groupe Marianas Trench?",
+//      o1: "Vancouver, Colombie-Britannique",
+//      o2: "Montréal, Québec",
+//      o3: "Los Angeles, Californie",
+//      o4: "St-Petersburg, Floride",
+//      r: 1,
+//    },
+//    {
+//      q: "Quand est-ce que le groupe a débuté?",
+//      o1: "15 décembre 2008",
+//      o2: "5 novembre 2006",
+//      o3: "3 octobre 2005",
+//      r: 3,
+//    },
+//    {
+//      q: "Avec quelle maison de disque le groupe a signé ses albums ?",
+//      o1: "604 Records",
+//      o2: "BlueBush Records",
+//      o3: "Outerscope Records",
+//      r: 1,
 //   },
-//   {
-//     q: "Quand est-ce que le groupe a débuté?",
-//     o1: "15 décembre 2008",
-//     o2: "5 novembre 2006",
-//     o3: "3 octobre 2005",
-//     r: 3,
-//   },
-//   {
-//     q: "Avec quelle maison de disque le groupe a signé ses albums ?",
-//     o1: "604 Records",
-//     o2: "BlueBush Records",
-//     o3: "Outerscope Records",
-//     r: 1,
-//   },
-//   {
+//    {
 //     q: "Combien de membres ont présentement Marianas Trench?",
 //     o1: "4",
 //     o2: "2",
 //     o3: "5",
 //     o4: "3",
 //     r: 1,
-//   },
-//   {
-//     q: "Quelle chanson pop que Josh Ramsay a-t-il écrite?",
-//     o1: "Call Me Maybe, Carly Rae",
-//     o2: "Shape of You, Ed Sheeran",
-//     o3: "Happier, Marshmello ft.Bastille",
-//     o4: "Blank Space, Taylor Swift",
-//     r: 1,
-//   },
-//   {
-//     q:
-//       "Vrai ou faux: La chanson Astoria était inspirée par le film The Goonies ?",
-//     o1: "Vrai",
-//     o2: "Faux",
-//     r: 1,
-//   },
-//   {
-//     q: "Quelle chanson inclut un rappel aux titres de chansons précédents?",
-//     o1: "Fallout",
-//     o2: "Dearly Departed",
-//     o3: "One Love",
-//     r: 2,
-//   },
-//   {
-//     q: "Quelle chason possède les paroles « Boots n’ cats » ?",
-//     o1: "Shutter",
-//     o2: "Good to You",
-//     o3: "Pop 101",
-//     r: 3,
-//   },
-// ];
+//    },
+//    {
+//      q: "Quelle chanson pop que Josh Ramsay a-t-il écrite?",
+//      o1: "Call Me Maybe, Carly Rae",
+//      o2: "Shape of You, Ed Sheeran",
+//      o3: "Happier, Marshmello ft.Bastille",
+//      o4: "Blank Space, Taylor Swift",
+//      r: 1,
+//    },
+//    {
+//      q:
+//        "Vrai ou faux: La chanson Astoria était inspirée par le film The Goonies ?",
+//      o1: "Vrai",
+//      o2: "Faux",
+//      r: 1,
+//    },
+//    {
+//      q: "Quelle chanson inclut un rappel aux titres de chansons précédents?",
+//      o1: "Fallout",
+//      o2: "Dearly Departed",
+//      o3: "One Love",
+//      r: 2,
+//    },
+//    {
+//      q: "Quelle chanson possède les paroles « Boots n’ cats » ?",
+//      o1: "Shutter",
+//      o2: "Good to You",
+//      o3: "Pop 101",
+//      r: 3,
+//    },
+//  ];
 
 let modal = document.querySelector('.modal-body .quiz');
-fetch('https://khimmaloney.com.n0c.world/json/')
+fetch('https://khimmaloney.com.n0c.world/marianastrench-api/index.php/wp-json/wp/v2/posts')
 .then(response => response.json())
-.then(result => new Quiz(result.Questions));
+.then(result => new Quiz(result));
 
 class Quiz {
   constructor(tableau) {
@@ -82,10 +82,12 @@ class Quiz {
     this.div = document.createElement("div");
     this.div.classList.add("question");
     this.quizBody.appendChild(this.div);
+    console.log(Q.acf);
+
 
     //strong
     this.strong = document.createElement("strong");
-    this.strong.innerText = Q.q;
+    this.strong.innerText = Q.acf.q;
     this.div.appendChild(this.strong);
 
     //br
@@ -93,7 +95,7 @@ class Quiz {
     this.div.appendChild(this.br);
 
     //options
-    if ("o1" in Q) {
+    if ("o1" in Q.acf) {
       let radio = document.createElement("input");
       radio.setAttribute("type", "radio");
       radio.setAttribute("name", "question" + value);
@@ -101,14 +103,14 @@ class Quiz {
       this.div.appendChild(radio);
 
       let label = document.createElement("label");
-      label.innerText = Q.o1;
+      label.innerText = Q.acf.o1;
       this.div.appendChild(label);
 
       this.br = document.createElement("br");
       this.div.appendChild(this.br);
     }
 
-    if ("o2" in Q) {
+    if ("o2" in Q.acf) {
       let radio = document.createElement("input");
       radio.setAttribute("type", "radio");
       radio.setAttribute("name", "question" + value);
@@ -116,14 +118,14 @@ class Quiz {
       this.div.appendChild(radio);
 
       let label = document.createElement("label");
-      label.innerText = Q.o2;
+      label.innerText = Q.acf.o2;
       this.div.appendChild(label);
 
       this.br = document.createElement("br");
       this.div.appendChild(this.br);
     }
 
-    if ("o3" in Q) {
+    if ("o3" in Q.acf) {
       let radio = document.createElement("input");
       radio.setAttribute("type", "radio");
       radio.setAttribute("name", "question" + value);
@@ -131,7 +133,7 @@ class Quiz {
       this.div.appendChild(radio);
 
       let label = document.createElement("label");
-      label.innerText = Q.o3;
+      label.innerText = Q.acf.o3;
       this.div.appendChild(label);
 
       this.br = document.createElement("br");
@@ -272,4 +274,3 @@ class Quiz {
   }
 }
 
-new Quiz(questions);
